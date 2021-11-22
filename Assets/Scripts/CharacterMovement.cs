867e9527b2ef5b35ecc82b5ac7c0a6b51ed3eb76
Mateen Aminian <maaminia@ucsc.edu>
2021-11-21 19:36:51 -0800

    Added tilemap and player movement
    Mouse click on a position to move to within 3 tiles of player.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Tilemap map;
+    MouseInput mouseInput;
+    private Vector3 destination;
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private void Awake() {
+        mouseInput = new MouseInput();
+    }
+
+    private void OnEnable(){
+        mouseInput.Enable();
+    }
+
+    private void OnDisable(){
+        mouseInput.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform.position;
+        mouseInput.Mouse.MouseClick.performed += _ => MouseClick();
+    }
+
+    private void MouseClick() {
+        Vector2 mousePos = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        // make sure we are clicking the cell
+        Vector3Int gridPos = map.WorldToCell(mousePos);
+        //if (map.HasTile(gridPos)){
+        destination = mousePos;
+        //Debug.Log(destination);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, destination) > 0.1f && Vector3.Distance(transform.position, destination) < 3.0f){
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
+       // Debug.Log(mousePos);
+    }
+}
