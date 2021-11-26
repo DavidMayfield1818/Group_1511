e4afabf6ba63c445806475a362723fe726ba8782
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2021-11-26 15:48:25 -0800

    small edit to distancetoHoop

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -35,7 +35,7 @@ public class Guard : MonoBehaviour
     }
 
     public void steal(){
-        //if this player is one space from enemyPlayer
+        //if this player is one hex from enemyPlayer
         //run stealchance
         //if successful this player has possession of the ball
         //maybe resets movement?
@@ -43,7 +43,7 @@ public class Guard : MonoBehaviour
 
     public void distancetoHoop(){
         //flat 10% if shooting from the other side of the court
-        //40% at the 3 point line
+        //40% at the 3 point line (4 hexes distance)
         //80% at the hoop (layup)
         //80%-8*(amount of hexes away from hoop)
         //shotChance = finishedcalculation
